b4aa42e9a11d8c1f13cdf3ac395ec3d19a26b3ff
Geoff Schorkopf <gschorkopf@gmail.com>
2015-05-04 16:38:09 -0400

    Routes literally anything to the log method

--- lib/lita/handlers/gimli.rb
+++ lib/lita/handlers/gimli.rb
@@ -1,6 +1,10 @@
 module Lita
   module Handlers
     class Gimli < Handler
+      route %r{(.+)}, :log
+
+      def log(response)
+      end
     end
 
     Lita.register_handler(Gimli)
--- spec/lita/handlers/gimli_spec.rb
+++ spec/lita/handlers/gimli_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
 
 describe Lita::Handlers::Gimli, lita_handler: true do
+  it { is_expected.to route("Anything").to(:log) }
 end
